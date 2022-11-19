1ac6a6b24500bdd6cc3229e3d479464ed1fb599b
Daniel-Quintero1 <daqcarrillo@gmail.com>
2022-11-18 23:56:50 -0300

    modificacion app

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,10 +4,10 @@ function Card(props) {
    return (
       <div>
          <button className={stilo.buttonInicio} onClick={props.onClose} >X</button>
-         <h2 className={stilo.tittleTowlabel} >{props.name}</h2>
+         <h2 className={stilo.tittleh2} >{props.name}</h2>
          <h2>{props.especies}</h2>
-         <h2>{props.gender}</h2>
-         <img  src={props.image} alt={props.name} />
+         <h2 className={stilo.tittleh3}>{props.gender}</h2>
+         <img  className={stilo.imagenh1} src={props.image} alt={props.name} />
       </div>
    );
 }
